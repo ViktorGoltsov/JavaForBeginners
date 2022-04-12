a7ebd9b57d9cf09c3cb3eed301d723336c0acb4a
Victor <victor.goltsov@gmail.com>
2022-04-13 01:46:15 +0800

    4122022

--- src/homeWork/HW5.java
+++ src/homeWork/HW5.java
@@ -12,7 +12,7 @@ public class HW5 {
     }
 
     //ВСЕ!!! результаты должны быть протестированы, для этого необходимо создать метод с названием verifyEquals(expectedResult, actualResult)
-    public static void verifyEquals (String expectedResult, String actualResult) {
+    public static void verifyEquals(String expectedResult, String actualResult) {
         if (expectedResult.equalsIgnoreCase(actualResult)) {
             System.out.println("\u001B[32m" + "pass" + "\u001B[0m");
         } else {
@@ -20,25 +20,25 @@ public class HW5 {
         }
     }
 
-    public static void verifyEquals (int expectedResult, int actualResult) {
-        if (expectedResult ==actualResult) {
+    public static void verifyEquals(int expectedResult, int actualResult) {
+        if (expectedResult == actualResult) {
             System.out.println("\u001B[32m" + "pass" + "\u001B[0m");
         } else {
             System.out.println("\u001B[31m" + "fail " + "\u001B[0m");
         }
     }
 
-    public static void verifyEquals (double expectedResult, double actualResult) {
-        if (expectedResult ==actualResult) {
+    public static void verifyEquals(double expectedResult, double actualResult) {
+        if (expectedResult == actualResult) {
             System.out.println("\u001B[32m" + "pass" + "\u001B[0m");
         } else {
             System.out.println("\u001B[31m" + "fail " + "\u001B[0m");
         }
     }
 
-      //1.2. Написать метод, который принимает на вход число от 1 до 7  и возвращает день недели.
+    //1.2. Написать метод, который принимает на вход число от 1 до 7  и возвращает день недели.
 
-    public static String dayOfWeek (int dayNum) {
+    public static String dayOfWeek(int dayNum) {
         String day = "";
 
         if (dayNum == 1) {
@@ -67,14 +67,15 @@ public class HW5 {
     }
 
     //1.3.
-    public static int largestValue (int x, int y, int z) {
+    public static int largestValue(int x, int y, int z) {
         int largest;
 
         if (x > y) {
             largest = x;
         } else {
             largest = y;
-        } if (z > largest) {
+        }
+        if (z > largest) {
             largest = z;
         }
 
@@ -82,26 +83,46 @@ public class HW5 {
     }
 
     //1.4.
-    public static int smallestValue (int a, int b, int c) {
+    public static int smallestValue(int a, int b, int c) {
         int smallest;
+//        if (a < b) {
+//            if (c < a) {
+//                smallest = c;
+//            } else {
+//                smallest = a;
+//            }
+//            return smallest;
+//        } else if (c < b) {
+//            smallest = c;
+//        } else {
+//            smallest = b;
+//        }
+//        return smallest;
 
-        if (b < c) {
-            smallest = b;
+        if (a < b) {
+            smallest = a;
         } else {
+            smallest = b;
+        } if (c < smallest) {
             smallest = c;
         }
-
-        if (a < smallest) {
-            smallest = a;
-        }
         return smallest;
+
 
     }
 
     //1.5. Написать алгоритм вычисления среднего значения из 5 показателей температуры тела кота.
-    public static String tempOfCat (double t1, double t2, double t3, double t4, double t5) {
+    public static String tempOfCat(double t1, double t2, double t3, double t4, double t5) {
+        String avg = "";
+        double tmin = 34.0;
+        double tmax = 39.0;
+        if ((t1 <= tmax && t2 <= tmax && t3 <= tmax && t4 <= tmax && t5 <= tmax)
+                && (t1 >= tmin && t2 >= tmin && t3 >= tmin && t4 >= tmin && t5>= tmin)) {
         double avg1 = (t1 + t2 + t3 + t4 + t5) / 5;
-        String avg = String.format("%.2f",avg1);
+        avg = String.format("%.2f", avg1);
+        } else {
+        avg = "Температура за пределами диапазона";
+        }
         return avg;
     }
 
@@ -115,16 +136,16 @@ public class HW5 {
 
         //1.3.
         task();
-        int x = 5;
-        int y = 7;
-        int z = 12;
+        int x = 15;
+        int y = 10;
+        int z = 1;
         System.out.println(largestValue(x, y, z));
-        verifyEquals(12, largestValue(x,y,z));
+        verifyEquals(15, largestValue(x, y, z));
 
         // 1.4.
         task();
-        System.out.println(smallestValue(x,y,z));
-        verifyEquals(5, smallestValue(x, y,z));
+        System.out.println(smallestValue(x, y, z));
+        verifyEquals(1, smallestValue(x, y, z));
 
         //1.5.
         task();
@@ -135,9 +156,9 @@ public class HW5 {
         double t5 = 38.5;
         System.out.println(tempOfCat(t1, t2, t3, t4, t5));
         verifyEquals("37.24", String.valueOf(tempOfCat(t1, t2, t3, t4, t5)));
-
-
+        verifyEquals("Температура за пределами диапазона", String.valueOf(tempOfCat(50, t2, t3, t4, t5)));
 
     }
+
 
 }
