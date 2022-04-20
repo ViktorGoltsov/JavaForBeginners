2e63a9d9ab9a4e9f667582d7f0923bc3e249bf00
Victor <victor.goltsov@gmail.com>
2022-04-21 00:56:05 +0800

    04132022

--- src/homeWork/HW6.java
+++ src/homeWork/HW6.java
@@ -73,7 +73,136 @@ public class HW6 {
         }
     }
 
+    // 9. Распечатать последовательность чисел в промежутке от -10 до 34 включительно. Числа, кратные 11,
+    // должны быть распечатаны синим цветом. Числа, кратные 12, должны быть распечатаны красным цветом.
+    // А ноль необходимо распечатать словом ZERO.
 
+    public static void sequence9() {
+        for (int i = -10; i <= 34; i++) {
+
+            if ((i % 11 == 0) && (i != 0)) {
+                System.out.println("\u001B[34m" + i + "\u001B[0m"); //blue
+            }  else if ((i % 12 == 0) && (i != 0)) {
+                System.out.println("\u001B[31m" + i + "\u001B[0m"); //red
+            }  else if (i == 0) {
+                System.out.println("ZERO");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void startEndStep (int start, int end, int step) {
+        for (int i = start; i < end; i += step) {
+            System.out.println((double) i);
+        }
+    }
+
+    public static void sequence11 (int l) {
+        int lenth = 0;
+        for (int i = 0; lenth < l; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                lenth = lenth + 1;
+            }
+        }
+    }
+
+    //12 Напишите метод, который принимает целое число n, и выводит все степени числа 2 от 1 до n включительно
+    public static void pow (int n) {
+        for (int i = 1; i <= n;  i++) {
+            System.out.println((int)Math.pow(2, i));
+        }
+    }
+
+    //13 Вывести последовательность 012345678900112233445566778899000…  до числа 9999 включительно.
+    public static void sequence13() {
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                int l = 1;
+                while (l <= i) {
+                    System.out.print(j);
+                    l++;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    //14 Распечатайте последовательность чисел:
+    //0, 1, -1, 2, -2, 3, -3, 4, -4, 5, -5
+    public static void sequence14 () {
+        int i = 0;
+        while (Math.abs(i) < 6) {
+            if (i == 0) {
+                System.out.println(i);
+                i++;
+            } else if (i > 0) {
+                System.out.println(i);
+                i = i * (-1);
+            } else {
+                System.out.println(i);
+                i = i * (-1) + 1;
+            }
+        }
+    }
+
+    //16 Написать метод, который принимает параметры n, m, , и печатает последовательность нечетных чисел начиная
+    // с числа n, с шагом m,  длина последовательности l.
+
+    public static void sequence16(int n, int m, int l) {
+        if (n % 2 == 0) {
+            n = n + 1;
+        }
+        int lon = 0;
+        for (int i = n; lon < l  ; i+=m) {
+            System.out.println(i);
+            lon = lon + 1;
+        }
+    }
+
+    //17. Сгенерируйте и распечатайте последовательность по формуле:
+    //n + 1 = n + 2
+
+    public static void sequence17 (int n, int l) { //n - начало последовательности l - количество членов последовательности
+        for (int i = 1; i <= l; i++) {
+            System.out.println(n);
+            n += 2;
+        }
+    }
+
+    //18. Написать метод, который принимает параметры l, n, и печатает последовательность чисел, начиная с числа n,
+    // по формуле для n + 1 члена последовательности: n + 1 = 2n   Длина последовательности  - l .
+    public static void sequence18 (int n, int l) {
+
+        for (int i = 1; i <= l; i++) {
+            System.out.println(n);
+            n *= 2;
+        }
+    }
+
+    //19 Сгенерируйте последовательность целых положительных  двузначных чисел, в которых разница между первой цифрой
+    // (десятки) и второй цифрой (единицы) не превышает 3.
+    public static void sequence19 () {
+        for (int i = 10; i < 100; i++) {
+            if (((int)(i / 10) - (int)(i % 10)) <= 3) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    //20
+    public static double function (double x) {
+        double y;
+        if (x > 1.5) {
+            y = 2.5 * Math.pow(x, 3) + 6 * Math.pow(x, 2) - 30;
+        } else if (x >= 0 && x <= 1.5) {
+            y = x + 1;
+        } else {
+            y = x;
+        }
+        return y;
+    }
 
     public static void main(String[] args) {
         //1
@@ -107,5 +236,56 @@ public class HW6 {
         //8.
         task();
         sequence8();
+
+        //9.
+        task();
+        sequence9();
+
+        //10.
+        task();
+        startEndStep(5,15,2);
+
+        //11.
+        task();
+        sequence11(5);
+
+        //12.
+        task();
+        pow(5);
+
+        //13.
+        task();
+        sequence13();
+
+        //14.
+        task();
+        sequence14();
+
+        //15
+        task();
+
+        //16
+        task();
+        sequence16(6,3, 4);
+
+        //17.
+        task();
+        sequence17(5, 4);
+
+        task();
+        sequence18(5, 4);
+
+        task();
+        sequence19();
+
+        task();
+        System.out.println(function(1.6));
+        System.out.println(function(1.5));
+        System.out.println(function(0));
+        System.out.println(function(-1));
+        System.out.println(function(-0.1));
+        System.out.println(function(1));
+        System.out.println(function(2));
     }
+
 }
